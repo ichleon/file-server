c280a73b30b61009f284603f2d0577e40a3bbf88
ichleon <252365433+ichleon@users.noreply.github.com>
2026-09-25 16:08:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def delete_file(filename):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
